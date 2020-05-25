4d52fffa96faf99f09774f76c3663437a67af4e1
John Afolabi <johnafolabi.da@gmail.com>
2020-05-25 16:22:46 +0100

    Complete contains_dupe javascript

--- contains-dupes/contains_dupe.js
+++ contains-dupes/contains_dupe.js
@@ -1,3 +1,14 @@
-var containsDuplicate = function(nums) {
+var containsDuplicate = function (nums) {
+	const d = {};
+	duplicate = false;
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] in Object.keys(d)) {
+			duplicate = true;
+		} else {
+			d[nums[i]] = 1;
+		}
+	}
+	return duplicate;
+};
 
-}
+console.log(containsDuplicate([1, 2, 3, 4]));
